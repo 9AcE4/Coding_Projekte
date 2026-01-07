f9e2676a937e12f493aa847bbd4dc48b002a9110
Me_Py <p.arndt2@gmx.de>
2026-01-07 14:29:30 +0100

    Update rat_die_zahl.py
    Aktuell nur im Terminal

--- python/minigames/rat_die_zahl.py
+++ python/minigames/rat_die_zahl.py
@@ -1,1 +1,29 @@
 # Zahlenraten
+
+from random import random
+
+print("Willkommen zum Zahlenratespiel!")
+win_number = int(random() * 10) + 1
+
+while True:
+    guess = int(input("Rate eine Zahl zwischen 1 und 10: "))
+
+    if guess < win_number:
+        print("Die gesuchte Zahl ist größer.")
+    elif guess > win_number:
+        print("Die gesuchte Zahl ist kleiner.")
+
+    else:
+        if guess == win_number:
+            print("Herzlichen Glückwunsch! Du hast die richtige Zahl erraten.")
+        
+            new_game = input("Möchtest du noch einmal spielen? (ja/nein): ")
+        
+        if new_game == 'ja':
+            win_number = int(random() * 10) + 1
+            continue
+        elif new_game == 'nein':
+            break
+
+
+print("Spiel beendet.")
